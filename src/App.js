69e6edfd44c9cfda05883ae5915147e09080b954
Luka Juroš <lkjuro@gmail.com>
2023-03-29 09:05:29 +0200

    13th commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import "./App.css";
 import PDF_1 from "./Oblikovanje_Osvrt_1.pdf";
 import PDF_2 from "./Oblikovanje_Osvrt_2.pdf";
-import ZIP_1 from "./vjezba1.zip";
+import ZIP_1 from 
 
 function App() {
+
+  const zipFileUrl = "https://file.io/E9L6aOzsarVI";
+  
   return (
     <div className="App">
       <header className="App-header">
@@ -24,12 +27,7 @@ function App() {
         >
           Predavanje_02
         </a>
-        <a
-          className="App-link"
-          target="_blank"
-          rel="noopener noreferrer"
-          href={ZIP_1}
-        >
+        <a href={zipFileUrl} download="vjezba1.zip">
           Predavanje_03
         </a>
       </header>
