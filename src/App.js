f076a09e01f570c57591d6e3a5801b3691578444
Luka Juroš <lkjuro@gmail.com>
2023-03-29 09:08:31 +0200

    15th commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import "./App.css";
 import PDF_1 from "./Oblikovanje_Osvrt_1.pdf";
 import PDF_2 from "./Oblikovanje_Osvrt_2.pdf";
-import ZIP_1 from 
 
 function App() {
-
   const zipFileUrl = "https://file.io/E9L6aOzsarVI";
-  
+
   return (
     <div className="App">
       <header className="App-header">
